d4f622669efd6fb5dea2cb78ebca414b7cf5b624
Dan <zxdan99@gmail.com>
2021-08-25 15:39:19 -0500

    Proyecto 1
    Código base

---
+++ evaluacion_bomba_1.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8,9,10,11,12,13);
+void setup()
+{
+  lcd.begin(16, 2);
+  task1();
+  task2(); 
+}
+
+void task1(){
+  
+}
+
+void task2(){
+  
+}
+
+void loop()
+{
+  task1();
+  task2();
+}
